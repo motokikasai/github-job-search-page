9401651124470ff38da9476e7da71646a64034da
motokikasai <kmomoky@hotmail.com>
2020-10-03 23:42:57 +0200

    Set up basic logic for fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import useFetchJobs from './hooks/useFetchJobs';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
+  const { jobs, loading, error } = useFetchJobs();
+
   return (
-    <div className='App'>
-      <h1>hello gh job search page!</h1>
-    </div>
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Refreshing.</h1>}
+      <h1># of job openings as of NOW: {jobs.length}</h1>
+    </Container>
   );
 };
 
---
+++ src/hooks/useFetchJobs.js
@@ -0,0 +1,7 @@
+export default function useFetchJobs(params, page) {
+  return {
+    jobs: [1, 2, 3],
+    loading: true,
+    error: true,
+  };
+}
