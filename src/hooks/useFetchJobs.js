bcdd434606e0ae1f679ac11c575f7e67a31bf9a7
motokikasai <kmomoky@hotmail.com>
2020-10-04 00:09:37 +0200

    Implement state logic via useReducer

--- src/hooks/useFetchJobs.js
+++ src/hooks/useFetchJobs.js
@@ -1,4 +1,34 @@
+import { useReducer } from 'react';
+
+const ACTIONS = {
+  MAKE_REQUEST: 'make-request',
+  GET_DATA: 'get-data',
+  ERROR: 'error',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.MAKE_REQUEST:
+      return { loading: true, jobs: [] };
+    case ACTIONS.GET_DATA:
+      return { ...state, loading: false, jobs: action.payload.jobs };
+    case ACTIONS.ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        jobs: [],
+      };
+    default:
+      return state;
+  }
+}
+
 export default function useFetchJobs(params, page) {
+  const { state, dispatch } = useReducer(reducer, { jobs: [], loading: true });
+
+  dispatch({ type: 'hello', payload: { x: 3 } });
+
   return {
     jobs: [1, 2, 3],
     loading: true,
