f96ce9578e4c7cebb20d7fd6ce0ec3e871ccdac6
motokikasai <kmomoky@hotmail.com>
2020-10-05 23:38:35 +0200

    Add pagination with Bootstrap - basic

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import useFetchJobs from './hooks/useFetchJobs';
 import Job from './components/Job';
 import { Container } from 'react-bootstrap';
+import JobsPagination from './components/JobsPagination';
 
 const App = () => {
   const [params, setParams] = useState({});
@@ -10,12 +11,14 @@ const App = () => {
 
   return (
     <Container className='my-4'>
-      <h1>GitHub Jobs</h1>
+      <h1 className='mb-4'>GitHub Jobs</h1>
+      <JobsPagination page={page} setPage={setPage} />
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try Refreshing.</h1>}
       {jobs.map(job => {
         return <Job key={job.id} job={job} />;
       })}
+      <JobsPagination page={page} setPage={setPage} />
     </Container>
   );
 };
